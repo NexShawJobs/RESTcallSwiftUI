ff5ecf6899325cef42005fbe9fe0352c4f111893
Neb <nex.shaw.jobs@gmail.com>
2022-05-12 14:11:52 -0400

    Initial Commit

---
+++ RESTcallSwiftUI/RESTcallSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTcallSwiftUIApp.swift
+//  RESTcallSwiftUI
+//
+//  Created by NebSha on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RESTcallSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
