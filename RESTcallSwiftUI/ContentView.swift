7924f9cbd5f0937172864164592a46eaf5f03ba1
Neb <nex.shaw.jobs@gmail.com>
2022-05-12 15:45:15 -0400

    initial commit

--- RESTcallSwiftUI/ContentView.swift
+++ RESTcallSwiftUI/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var simpleText: String = ""
+    @State private var statusCode: String = ""
     var body: some View {
         Text(simpleText)
+        Text(statusCode)
         Button {
             Task {
                 let (data, _) = try await URLSession.shared.data(from: URL(string:"https://qni31qs1i7.execute-api.us-east-1.amazonaws.com/pingPongStage/pingpongresource")!)
                 let decodedResponse = try? JSONDecoder().decode(SimpleText.self, from: data)
                 simpleText = decodedResponse?.body ?? "_"
+                statusCode = "\(decodedResponse?.statusCode ?? 0)"
             }
         } label: {
             Text("Simple Text")
@@ -30,4 +33,5 @@ struct ContentView_Previews: PreviewProvider {
 }
 struct SimpleText: Codable {
     let body: String
+    let statusCode: Int
 }
