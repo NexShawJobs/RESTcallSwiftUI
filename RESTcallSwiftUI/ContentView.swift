c43480db566d1ac48668c3569bb37ac588056abc
Neb <nex.shaw.jobs@gmail.com>
2022-05-12 18:21:31 -0400

    added simple post and get request

--- RESTcallSwiftUI/ContentView.swift
+++ RESTcallSwiftUI/ContentView.swift
@@ -11,19 +11,50 @@ import SwiftUI
 struct ContentView: View {
     @State private var simpleText: String = ""
     @State private var statusCode: String = ""
+        
     var body: some View {
         Text(simpleText)
         Text(statusCode)
         Button {
             Task {
+                
+                let json = "{ \"ping\" : \"nex\" }"
+                let url :URL = URL(string: "https://qni31qs1i7.execute-api.us-east-1.amazonaws.com/pingPongStage/pingpongresource")!
+                var request = URLRequest(url: url)
+                request.httpMethod = "Post"
+                request.httpBody = json.data(using: .utf8)
+                let session = URLSession(configuration: .default)
+                let task = session.dataTask(with: request) {
+                    (data, response, error) in
+                    
+                    if let data = data {
+                        if let postResponse = String(data: data, encoding: .utf8){
+                            print(postResponse)
+                            simpleText = postResponse
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                simpleText = ""
+                            }
+                        }
+                    }
+                }
+                task.resume()
+
+                
                 let (data, _) = try await URLSession.shared.data(from: URL(string:"https://qni31qs1i7.execute-api.us-east-1.amazonaws.com/pingPongStage/pingpongresource")!)
                 let decodedResponse = try? JSONDecoder().decode(SimpleText.self, from: data)
-                simpleText = decodedResponse?.body ?? "_"
+                //simpleText = decodedResponse?.body ?? "_"
                 statusCode = "\(decodedResponse?.statusCode ?? 0)"
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    statusCode = ""
+                }
             }
         } label: {
             Text("Simple Text")
         }
+        Text ("Time Taken:")
+        
+
+        
     }
 }
 struct ContentView_Previews: PreviewProvider {
